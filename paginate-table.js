21e069170aabff202cc6e6eceff508e19f353268
wangjue6 <wangjue6@spc-wangjue6.hikvision.com>
2016-02-17 14:45:06 +0800

    无数据时隐藏表格

--- paginate-table.js
+++ paginate-table.js
@@ -1,45 +1,55 @@
 (function($, window){
 	$.fn.table = function(options) {
-		var $instance = $(this);
-		var defaults = {
-			tableTmpl: 'tmpl-table',
-			paginateTmpl: 'tmpl-paginate',
-			fetch: function(e, params) {
-				var request = $.extend(settings.request, $instance.data('request'), params);
-				$.getJSON(settings.url, request).done(function(response) {
-					$instance.trigger('render', response);
-					$instance.data('request', request);
+		return this.each(function() {
+			var $instance = $(this);
+			var defaults = {
+				tableTmpl: 'tmpl-table',
+				paginateTmpl: 'tmpl-paginate',
+				fetch: function(e, params) {
+					var request = $.extend(settings.request, $instance.data('request'), params);
+					$.getJSON(settings.url, request).done(function(response) {
+						$instance.trigger('render', response);
+						$instance.data('request', request);
+					});
+				},
+				render: function(e, data) {
+					$instance.trigger('will-render', data);
+					
+					var $table = $instance.find('.table');
+					var $paginate = $instance.find('.paginate');
+					
+					if(data.page.itemCount > 0) {
+						$table.html(tmpl(settings.tableTmpl, data.page)).data("list", data.page.items);
+						$paginate.html(tmpl(settings.paginateTmpl, data.page));
+						$instance.show();
+					} else {
+						$instance.hide();
+					}
+					
+					$instance.trigger('rendered');
+				}
+			};
+			var settings = $.extend(defaults, $instance.data(), options);
+			
+			$instance.on('fetch', settings.fetch)
+					 .on('render', settings.render);
+			
+			$instance.on('submit', '.tmpl-paginate form', function(e) {
+				e.preventDefault();
+				var $this = $(this);
+				$instance.trigger('fetch', {
+					'pageNumber': $this.find('.input-page-number').val()
 				});
-			},
-			render: function(e, data) {
-				$instance.trigger('will-render', data);
-				$instance.find('.table').html(tmpl(settings.tableTmpl, data.page)).data("list", data.page.items);
-				$instance.find('.paginate').html(tmpl(settings.paginateTmpl, data.page));
-				$instance.trigger('rendered');
-			}
-		};
-		var settings = $.extend(defaults, $instance.data(), options);
-		
-		$instance.on('fetch', settings.fetch)
-				 .on('render', settings.render);
-		
-		$instance.on('submit', '.tmpl-paginate form', function(e) {
-			e.preventDefault();
-			var $this = $(this);
-			$instance.trigger('fetch', {
-				'pageNumber': $this.find('.input-page-number').val()
+			}).on('click', '.tmpl-paginate .goto-page', function(e) {
+				e.preventDefault();
+				var $this = $(this);
+				$instance.trigger('fetch', {
+					'pageNumber': $this.data('pageNumber')
+				});
 			});
-		}).on('click', '.tmpl-paginate .goto-page', function(e) {
-			e.preventDefault();
-			var $this = $(this);
-			$instance.trigger('fetch', {
-				'pageNumber': $this.data('pageNumber')
-			});
+			
+			$instance.trigger('fetch');
 		});
-		
-		$instance.trigger('fetch');
-		
-		return $instance;
 	};
 	
 	$('[role="paginate-table"]').table();
